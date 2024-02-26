6b968097eb1a88137732fb92f6d30d44830fee29
Alin Grigorescu <alin.grig87@gmail.com>
2024-02-26 20:41:30 +0200

    finish js-ws01 - part 1

--- JS/WORKSHOP/01-Calculator/app.js
+++ JS/WORKSHOP/01-Calculator/app.js
@@ -10,3 +10,39 @@ function add() {
 
 	output.innerHTML = firstNumber + ' + ' + secondNumber + ' = ' + sum;
 }
+
+function subtract() {
+	var firstNumber = Number(firstNumberInput.value);
+	var secondNumber = Number(secondNumberInput.value);
+
+	var res = firstNumber - secondNumber;
+
+	output.innerHTML = firstNumber + ' - ' + secondNumber + ' = ' + res;
+}
+
+function multiply() {
+	var firstNumber = Number(firstNumberInput.value);
+	var secondNumber = Number(secondNumberInput.value);
+
+	var res = firstNumber * secondNumber;
+
+	output.innerHTML = firstNumber + ' x ' + secondNumber + ' = ' + res;
+}
+
+function divide() {
+	var firstNumber = Number(firstNumberInput.value);
+	var secondNumber = Number(secondNumberInput.value);
+
+	var res = firstNumber / secondNumber;
+
+	output.innerHTML = firstNumber + ' / ' + secondNumber + ' = ' + res;
+}
+
+function modulus() {
+	var firstNumber = Number(firstNumberInput.value);
+	var secondNumber = Number(secondNumberInput.value);
+
+	var res = firstNumber % secondNumber;
+
+	output.innerHTML = firstNumber + ' % ' + secondNumber + ' = ' + res;
+}
